600654f6a7d6c918cebec8ea3d0306b6170047fb
Jean Anderson <jeanandersom1@gmail.com>
2023-04-23 17:22:17 -0300

    cronometro_c

---
+++ cronometro.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int s = 0;
+int m = 0;
+int h = 0;
+char letra;
+
+int main()
+{
+    printf("=============================================")
+    printf("                 cronometro")
+    printf("=============================================")
+    printf("Para Iniciar o Cronometro Aperte i: ");
+    scanf("%s",letra);
+    /*while(letra != "i"){
+        s++;
+        if(s == 60){
+            s=0;
+            m++;
+            if(m == 60){
+                m=0;
+                h++
+                if(h == 24){
+                    s = 0;
+                    m = 0;
+                    h = 0;
+                }
+            }
+            sleep(1000);
+        }
+        printf("%i : %i : %i\n",h,m,s);
+        
+        
+    }*/
+    return 0;
+}
